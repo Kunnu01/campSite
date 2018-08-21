d529eaa42c310d97d68caad57c72ea88cea42aef
kunnu01 <kunalbhatia2409@gmail.com>
2018-08-21 18:12:29 +0530

    With database

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 var app = express();
+const mongoose = require('mongoose');
 var port = process.env.PORT || 3000;
+mongoose.connect("mongodb://localhost:27017/yel_camp", (err, db) =>{
+    if (err) {
+        return console.log('Unable to connect to mongodb server');
+    }    
+    console.log('connected to mongodb server');
+});
+
+//Schema setup
+let campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//     name: "Granite Hill",
+//     image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f4c77aafefb7ba_340.jpg",
+//     description: "This is a huge granite hill, no bathrooms, no water. Beautiful granite!"
+// }, (err, campground) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(campground);
+//     }
+// });
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -9,21 +39,53 @@ app.get('/', (req, res) => {
     res.render("landing");
 });
 
-app.get('/campgrounds', (req, res) => {
-    var campgrounds = [
-        {name: "Salmon Creek", image: "https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b0144291f6c271a5efb7_340.jpg"},
-        {name: "Granite Hill", image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f4c77aafefb7ba_340.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f4c77aafefb7ba_340.jpg"}
-    ]
 
-    res.render("campgrounds", {campgrounds:campgrounds});
+//INDEX - show all campgrounds
+app.get('/campgrounds', (req, res) => {
+    //Get all the campgrounds from db
+    Campground.find({}, (err, allCampgrounds) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", {campgrounds:allCampgrounds});
+        }
+    })
 });
 
-app.post('/campgrounds', (req, res) => {
-    res.send("You hit");
-    //get data from form and add to campgrounds array
-    //redirect beck to campgrounds page
 
+// CREATE - add new to DB
+app.post('/campgrounds', (req, res) => {
+    //get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc}
+    //create new campground and save to db
+    Campground.create(newCampground, (err, newlycreated) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    })
+});
+
+
+// NEW - show form to create to new campground
+app.get('/campgrounds/new', (req, res) => {
+    res.render("new.ejs");
+});
+
+//SHOW - show more info about one campground
+app.get('/campgrounds/:id', (req, res) => {
+    //find campground with provided id
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("show", {campground: foundCampground})
+        }
+    });
 });
 
 app.listen(port, () => {
